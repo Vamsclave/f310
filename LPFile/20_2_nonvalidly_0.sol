82e23592f5526e30a3517e3962c8092a59b38dd6
Bluemana <38228997+Vamsclave@users.noreply.github.com>
2024-05-14 17:49:10 +0200

    asd

---
+++ LPFile/20_2_nonvalidly_0.sol
@@ -0,0 +1,186 @@
+Problem:    
+Rows:       20
+Columns:    61 (61 integer, 0 binary)
+Non-zeros:  120
+Status:     INTEGER OPTIMAL
+Objective:  z = 1690177 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 contraite_3_0
+                                 923           923             = 
+     2 contraite_5_0
+                                 410           410             = 
+     3 contraite_8_0
+                                 192           192             = 
+     4 contraite_19_0
+                                 743           743             = 
+     5 contraite_10_0
+                                1313          1313             = 
+     6 contraite_11_0
+                                 453                         455 
+     7 contraite_18_0
+                                1842                        1842 
+     8 contraite_6_0
+                                 449                         449 
+     9 contraite_4_0
+                                 837                         837 
+    10 contraite_0_0
+                                   0             0             = 
+    11 contraite_1_0
+                                   0             0             = 
+    12 contraite_2_0
+                                   0             0             = 
+    13 contraite_7_0
+                                   0             0             = 
+    14 contraite_9_0
+                                   0             0             = 
+    15 contraite_12_0
+                                   0             0             = 
+    16 contraite_13_0
+                                   0             0             = 
+    17 contraite_14_0
+                                   0             0             = 
+    18 contraite_15_0
+                                   0             0             = 
+    19 contraite_16_0
+                                   0             0             = 
+    20 contraite_17_0
+                                   0             0             = 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 quantity_e_0_4_0
+                    *              0             0               
+     2 quantity_e_4_0_0
+                    *              0             0               
+     3 quantity_e_0_12_0
+                    *              0             0               
+     4 quantity_e_12_0_0
+                    *              0             0               
+     5 quantity_e_1_7_0
+                    *              0             0               
+     6 quantity_e_7_1_0
+                    *              0             0               
+     7 quantity_e_1_19_0
+                    *              0             0               
+     8 quantity_e_19_1_0
+                    *              0             0               
+     9 quantity_e_2_13_0
+                    *              0             0               
+    10 quantity_e_13_2_0
+                    *              0             0               
+    11 quantity_e_3_19_0
+                    *              0             0               
+    12 quantity_e_19_3_0
+                    *            923             0               
+    13 quantity_e_3_5_0
+                    *              0             0               
+    14 quantity_e_5_3_0
+                    *              0             0               
+    15 quantity_e_4_6_0
+                    *           2530             0               
+    16 quantity_e_6_4_0
+                    *              0             0               
+    17 quantity_e_4_11_0
+                    *              0             0               
+    18 quantity_e_11_4_0
+                    *            453             0               
+    19 quantity_e_4_15_0
+                    *            602             0               
+    20 quantity_e_15_4_0
+                    *              0             0               
+    21 quantity_e_4_18_0
+                    *              0             0               
+    22 quantity_e_18_4_0
+                    *           1842             0               
+    23 quantity_e_5_15_0
+                    *              0             0               
+    24 quantity_e_15_5_0
+                    *            410             0               
+    25 quantity_e_6_7_0
+                    *              0             0               
+    26 quantity_e_7_6_0
+                    *              0             0               
+    27 quantity_e_6_17_0
+                    *           2979             0               
+    28 quantity_e_17_6_0
+                    *              0             0               
+    29 quantity_e_7_13_0
+                    *              0             0               
+    30 quantity_e_13_7_0
+                    *              0             0               
+    31 quantity_e_7_18_0
+                    *              0             0               
+    32 quantity_e_18_7_0
+                    *              0             0               
+    33 quantity_e_8_15_0
+                    *              0             0               
+    34 quantity_e_15_8_0
+                    *            192             0               
+    35 quantity_e_9_10_0
+                    *              0             0               
+    36 quantity_e_10_9_0
+                    *              0             0               
+    37 quantity_e_9_14_0
+                    *              0             0               
+    38 quantity_e_14_9_0
+                    *              0             0               
+    39 quantity_e_9_13_0
+                    *              0             0               
+    40 quantity_e_13_9_0
+                    *              0             0               
+    41 quantity_e_9_12_0
+                    *              0             0               
+    42 quantity_e_12_9_0
+                    *              0             0               
+    43 quantity_e_10_19_0
+                    *              0             0               
+    44 quantity_e_19_10_0
+                    *              0             0               
+    45 quantity_e_10_17_0
+                    *              0             0               
+    46 quantity_e_17_10_0
+                    *           1313             0               
+    47 quantity_e_12_14_0
+                    *              0             0               
+    48 quantity_e_14_12_0
+                    *              0             0               
+    49 quantity_e_12_15_0
+                    *              0             0               
+    50 quantity_e_15_12_0
+                    *              0             0               
+    51 quantity_e_13_17_0
+                    *              0             0               
+    52 quantity_e_17_13_0
+                    *              0             0               
+    53 quantity_e_14_17_0
+                    *              0             0               
+    54 quantity_e_17_14_0
+                    *           1666             0               
+    55 quantity_e_14_19_0
+                    *           1666             0               
+    56 quantity_e_19_14_0
+                    *              0             0               
+    57 quantity_e_14_16_0
+                    *              0             0               
+    58 quantity_e_16_14_0
+                    *              0             0               
+    59 quantity_e_15_19_0
+                    *              0             0               
+    60 quantity_e_19_15_0
+                    *              0             0               
+    61 quantity_e_16_16_0
+                    *              0             0               
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
